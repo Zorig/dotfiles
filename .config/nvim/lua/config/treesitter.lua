d18556674d5382750c89525013415a300b94daf5
Zorig <Zorig@users.noreply.github.com>
2022-04-19 12:41:05 +0800

    autotag support tsx

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -1,18 +1,18 @@
 local M = {}
 
 function M.setup()
-	require('nvim-treesitter.configs').setup {
-		ensure_installed = { "css", "html", "graphql", "javascript", "json", "lua", "typescript", "python" },
+	require("nvim-treesitter.configs").setup({
+		ensure_installed = { "css", "html", "graphql", "javascript", "json", "lua", "typescript", "python", "tsx" },
 		sync_install = false,
 		highlight = {
 			enable = true,
 			additional_vim_regex_highlighting = false,
-			disable = {"python"}
+			disable = { "python" },
 		},
 		indent = {
-			enable = true
-		}
-	}
+			enable = true,
+		},
+	})
 end
 
 return M
